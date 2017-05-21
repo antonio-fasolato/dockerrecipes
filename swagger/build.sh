2dac9d5058fe86bfccf6cc46f2625564d561f4c9
antonio.fasolato@gmail.com <antonio.fasolato@gmail.com>
2017-05-21 23:50:59 +0200

    Aggiunto swagger-ui e server per pubblicare i json

--- swagger/build.sh
+++ swagger/build.sh
@@ -1,3 +1,5 @@
 docker build -t swagger .
 
-docker run -p 8080:80 -v /home/tony/Documents/docker-data/swagger/log:/var/log/nginx -d swagger
+docker run -p 8080:80 -p 8081:81 -p 8082:82 -v /home/tony/Documents/docker-data/swagger/log:/var/log/nginx -v /home/tony/Documents/docker-data/swagger/data:/usr/swagger-data -d swagger
+
+docker run -p 8080:80 -p 8081:81 -v /home/tony/Documents/docker-data/swagger/log:/var/log/nginx -v /home/tony/Documents/docker-data/swagger/data:/usr/swagger-data -ti swagger sh
