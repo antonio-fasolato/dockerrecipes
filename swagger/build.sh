571cd3ad1f3ca2c9e755fdf5a08547e62e8d602e
antonio.fasolato@gmail.com <antonio.fasolato@gmail.com>
2017-05-22 04:20:20 -0400

    Aggiunto swagger-codegen e passato a alpine-java

--- swagger/build.sh
+++ swagger/build.sh
@@ -1,5 +1,7 @@
 docker build -t swagger .
 
-docker run -p 8080:80 -p 8081:81 -p 8082:82 -v /home/tony/Documents/docker-data/swagger/log:/var/log/nginx -v /home/tony/Documents/docker-data/swagger/data:/usr/swagger-data -d swagger
+docker run -p 8080:80 -p 8081:81 -p 8082:82 -v /home/tony/docker-data/swagger/logs:/var/log/nginx -v /home/tony/docker-data/swagger/data:/usr/swagger-data -d swagger
 
-docker run -p 8080:80 -p 8081:81 -v /home/tony/Documents/docker-data/swagger/log:/var/log/nginx -v /home/tony/Documents/docker-data/swagger/data:/usr/swagger-data -ti swagger sh
+docker run -p 8080:80 -p 8081:81 -p 8082:82 -v /home/tony/docker-data/swagger/logs:/var/log/nginx -v /home/tony/docker-data/swagger/data:/usr/swagger-data -ti swagger sh
+
+java -jar swagger-codegen-cli-2.2.2.jar generate -l html -i /usr/swagger-data/swagger.json  -o /usr/swagger-data
