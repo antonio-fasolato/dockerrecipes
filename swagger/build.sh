92054e12f30f31a1e495d7191f645c7d962806d4
antonio.fasolato@gmail.com <antonio.fasolato@gmail.com>
2017-05-26 04:03:46 -0400

    added script to always restart

--- swagger/build.sh
+++ swagger/build.sh
@@ -1,6 +1,7 @@
 docker build -t swagger .
 
 docker run -p 8080:80 -p 8081:81 -p 8082:82 -v /home/tony/docker-data/swagger/logs:/var/log/nginx -v /home/tony/docker-data/swagger/data:/usr/swagger-data -d swagger
+docker run -p 80:80 -p 81:81 -p 82:82 -v /home/tony/docker-data/swagger/logs:/var/log/nginx -v /home/tony/docker-data/swagger/data:/usr/swagger-data -d --restart always swagger
 
 docker run -p 8080:80 -p 8081:81 -p 8082:82 -v /home/tony/docker-data/swagger/logs:/var/log/nginx -v /home/tony/docker-data/swagger/data:/usr/swagger-data -ti swagger sh
 
